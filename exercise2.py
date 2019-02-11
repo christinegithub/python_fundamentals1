d89ef2db1bd32214331ad784c2232aeef3e10539
christinegithub <christine13@gmail.com>
2019-02-11 12:22:24 -0500

    exercise2

---
+++ exercise2.py
@@ -0,0 +1,23 @@
+#1. How would you calculate a good tip for a 55 dollar meal? Use print to print the answer.
+#
+# meal = 55
+# tip = 55 * 0.15
+#
+# print("The tip you should pay is {}".format(tip))
+
+#2. Try adding a string and an integer with the + operator. What happens? Find a way to
+#convert the integer into a string first and use print to print the result.
+
+# fruit = 'apple'
+# price = 2
+#
+# print(fruit + str(price))
+
+#3. Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+# print("45628 multiplied by 7839 is {}".format(45628 * 7839))
+
+#4. What's the value of the expression (10 < 20 and 30 < 20) or not(10 == 11)? Try figuring it out on your
+#own before typing it in.
+
+#True
