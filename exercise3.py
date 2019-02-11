b93291cea7f1c106d963dd92023cbca4e1409a92
christinegithub <christine13@gmail.com>
2019-02-11 13:07:20 -0500

    exercise3 completed

---
+++ exercise3.py
@@ -0,0 +1,21 @@
+# print("What is your name?")
+# user_name = input()
+# print("Hello, {}".format(user_name))
+
+# secret_password = "please"
+#
+# print("What is the password")
+#
+# password_attempt = input()
+#
+# correct_or_not = (password_attempt == secret_password)
+#
+# print("That is {}".format(correct_or_not))
+
+print("How old are you?")
+
+user_age = input()
+
+user_age_year = 2019 - int(user_age)
+
+print("Your birth year is {}".format(user_age_year))
